a4e71c431331af3a4e42352ee1f82ad47145fd31
James Scovern <jamie.scovern@gmail.com>
2016-06-20 11:20:05 -0600

    added prompt for email

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 //Javascript to be added
+
+window.onload = function() {
+
+	document.querySelector("#button-yellow").addEventListener("click", promptEmail);
+
+	function promptEmail () {
+		prompt("What is your email?");
+	}
+};
